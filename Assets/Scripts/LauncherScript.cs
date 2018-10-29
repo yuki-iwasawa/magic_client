5d4d26dc72cd7bad9ef9b441ce55a2fd92dc5eef
yuki-iwasawa <bravery1126@gmail.com>
2018-10-30 00:08:02 +0900

    Launcher画面作成

---
+++ Assets/Scripts/LauncherScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickButton() {
+        StartCoroutine(Connect());
+    }
+
+    // 戻り値は仮
+    private string Connect() {
+        // アカウントがあったら
+        // 接続確認
+        Debug.Log("ログインしました。");
+        return "";
+
+        // アカウントがなかったら
+        // アカウント作成画面へ
+
+    }
+}
+
