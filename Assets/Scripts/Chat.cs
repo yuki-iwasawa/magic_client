f3e37382c5d6c1564f3b6d7258fa15ad3f7b4c42
yuki-iwasawa <bravery1126@gmail.com>
2018-11-04 23:13:30 +0900

    websocket通信周りの設定

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+using System.Collections.Generic;
+
+public class Chat : MonoBehaviour
+{
+
+    WebSocket ws;
+
+    void Start()
+    {
+        //var dict = new Dictionary<string, object>();
+        //var identifier = new Dictionary<string, string>();
+        //identifier["channel"] = "ChatChannel";
+
+        //dict["command"] = "subscribe";
+
+        //dict["identifier"] = JsonUtility.ToJson(identifier); // 一度JSONにシリアライズしておく
+
+        ws = new WebSocket("ws://localhost:3000/cable");
+
+
+        ws.OnOpen += (sender, e) =>
+        {
+            var dict = new Dictionary<string, object>();
+            var identifier = new Dictionary<string, string>();
+            var data = new Dictionary<string, string>();
+            identifier["channel"] = "ChatChannel";
+
+            data["action"] = "speak";
+            data["message"] = "hello";
+
+            dict["command"] = "message";
+
+            dict["identifier"] = JsonUtility.ToJson(identifier); // 一度JSONにシリアライズしておく
+            dict["data"] = JsonUtility.ToJson(data);
+
+            ws.Send(JsonUtility.ToJson(dict));
+
+            Debug.Log("WebSocket Open");
+        };
+
+        //ws.Send(JsonUtility.ToJson(dict));
+
+        ws.OnMessage += (sender, e) =>
+        {
+            // e.Type
+            Debug.Log("WebSocket Message Type: " + ", Data: " + e.Data);
+        };
+
+        ws.OnError += (sender, e) =>
+        {
+            Debug.Log("WebSocket Error Message: " + e.Message);
+        };
+
+        ws.OnClose += (sender, e) =>
+        {
+            Debug.Log("WebSocket Close");
+        };
+
+        ws.Connect();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp("s"))
+        {
+            //var dict = new Dictionary<string, object>();
+            //var identifier = new Dictionary<string, string>();
+            //var data = new Dictionary<string, string>();
+            //identifier["channel"] = "ChatChannel";
+
+            //data["action"] = "speak";
+            //data["message"] = "hello";
+
+            //dict["command"] = "message";
+
+            //dict["identifier"] = JsonUtility.ToJson(identifier); // 一度JSONにシリアライズしておく
+            //dict["data"] = JsonUtility.ToJson(data);
+
+            //ws.Send(JsonUtility.ToJson(dict));
+
+            //dict["identifier"] = Json.Serialize(identifier); // 一度JSONにシリアライズしておく
+            //ws.Send(JSON.Serialize(dict));
+            ws.Send("Test Message");
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+}
---
+++ Assets/Scripts/ChatInput.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChatInput : MonoBehaviour
+{
+
+    public static InputField chatInputField;
+
+
+    /// <summary>
+    /// Startメソッド
+    /// InputFieldコンポーネントの取得および初期化メソッドの実行
+    /// </summary>
+    void Start()
+    {
+
+        chatInputField = GetComponent<InputField>();
+
+        InitInputField();
+    }
+
+
+
+    /// <summary>
+    /// Log出力用メソッド
+    /// 入力値を取得してLogに出力し、初期化
+    /// </summary>
+
+
+    public void InputLogger()
+    {
+
+        string userNameValue = chatInputField.text;
+
+        Debug.Log(userNameValue);
+
+        //InitInputField();
+    }
+
+
+
+    /// <summary>
+    /// InputFieldの初期化用メソッド
+    /// 入力値をリセットして、フィールドにフォーカスする
+    /// </summary>
+
+
+    void InitInputField()
+    {
+
+        // 値をリセット
+        chatInputField.text = "";
+
+        // フォーカス
+        chatInputField.ActivateInputField();
+    }
+
+
+    /// <summary>
+    /// Gets the input field.
+    /// </summary>
+    /// <returns>The input field.</returns>
+    public static InputField getChatInputField()
+    {
+        return chatInputField;
+    }
+
+}
