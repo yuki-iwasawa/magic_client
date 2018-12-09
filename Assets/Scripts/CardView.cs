e701284584e3f79b16481cce5a19f12c7e1d9c8c
yuki-iwasawa <bravery1126@gmail.com>
2018-12-09 21:05:45 +0900

    手札の数によってカードを生成する処理を追加する

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,43 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CardView : MonoBehaviour
+//{
+
+//    [SerializeField] private TextMeshProUGUI cardNameText;
+//    [SerializeField] private TextMeshProUGUI cardEffectText;
+
+//    private MstCards.Card cardData;
+
+//    /// <summary>
+//    /// Instantiate the specified cardData.
+//    /// </summary>
+//    /// <param name="cardData">Card data.</param>
+//    public void Instantiate(CardView prefab, Transform parent, MstCards.Card cardData)
+//    {
+//        //インスタンス化
+//        CardView item = Instantiate(prefab, parent).GetComponent<CardView>();
+//        //初期値設定
+//        item.cardData = cardData;
+//        //表示
+//        item.Print();
+//    }
+
+//    /// <summary>
+//    /// 自身(カード)を拡大して表示する
+//    /// </summary>
+//    public void Zoom()
+//    {
+//        //省略
+//    }
+
+//    /// <summary>
+//    /// カード情報を表示する
+//    /// </summary>
+//    public void Print()
+//    {
+//        cardNameText.text = this.cardData.name;
+//        cardEffectText.text = this.cardData.effect;
+//    }
+//}
---
+++ Assets/Scripts/HandCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandCard : MonoBehaviour {
+
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+
+        // prefab
+        prefab = Resources.Load<GameObject>("Card3");
+        for (int i = 1; i < 8; i++)
+        {
+            //var test = prefab.gameObject.GetComponent<GameObject>();()
+            GameObject obj = Instantiate(prefab, this.transform.position, Quaternion.identity, this.transform);
+            //obj.FindChild("CardTitle").gameObject.GetComponent<Text>().text = "abcdefghi";
+            //obj.transform.GetComponent<CardTitle>
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
