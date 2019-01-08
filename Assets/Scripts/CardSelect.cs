7946348c14bceb8114f77739187ffd66e3b84f26
yuki-iwasawa <bravery1126@gmail.com>
2019-01-08 15:11:18 +0900

    チャット画面の修正
    バトル画面のカードの挙動修正

--- Assets/Scripts/CardSelect.cs
+++ Assets/Scripts/CardSelect.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardSelect : MonoBehaviour {
+public class CardSelect : MonoBehaviour, IPointerEnterHandler
+{
+    private RectTransform rect;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector2 TopRight;
+    public Vector2 TopLeft;
+    public Vector2 BottomLeft;
+    public Vector2 BottomRight;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+
+        rect = gameObject.GetComponent<RectTransform>();
+
+        var offsetMax = rect.offsetMax;
+        var offsetMin = rect.offsetMin;
+
+        TopRight = new Vector2(Screen.width + offsetMax.x, Screen.height + offsetMax.y);
+        TopLeft = new Vector2(offsetMin.x, Screen.height + offsetMax.y);
+        BottomLeft = new Vector2(offsetMin.x, offsetMin.y);
+        BottomRight = new Vector2(Screen.width + offsetMax.x, offsetMin.y);
+    }
 }
